f19f4919683c2a71fa8955b64fe5f1ced640e0b8
taq-f <mysecondproject@gmail.com>
2018-07-19 23:37:28 +0900

    add starter kit

---
+++ index.ts
@@ -0,0 +1,5 @@
+import Vue from 'vue';
+import Hello from './vue/Hello.vue';
+
+const app = new Vue(Hello);
+app.$mount('#app');
---
+++ vue/Hello.vue
@@ -0,0 +1,23 @@
+<template>
+  <p>
+    {{ greeting }} World!!
+  </p>  
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+
+@Component
+export default class Hello extends Vue {
+  greeting = "hello, ";
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+  color: #2e9afe;
+}
+</style>
